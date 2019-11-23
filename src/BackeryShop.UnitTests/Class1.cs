cc9f82cf8e39d25b9f9356dc50ca0af54cad276b
shahed mahmoudi <S.Mahmoudi@live.com>
2019-11-23 13:43:52 +0100

    Add project files.

---
+++ src/BackeryShop.UnitTests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using BackeryShop;
+using NUnit.Framework;
+
+namespace BuildEng.UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void init()
+        {
+            Item obj = new Item();
+            int result=obj.Add(11, 20);
+            Assert.AreEqual(31, result);
+        }
+    }
+}
